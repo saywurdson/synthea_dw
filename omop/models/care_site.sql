ba7cca5fb8e25273a333059b34e98d61bfae552d
saywurdson <34385409+saywurdson@users.noreply.github.com>
2024-01-06 02:45:31 -0500

    update care_site model

--- omop/models/care_site.sql
+++ omop/models/care_site.sql
@@ -1,10 +1,26 @@
 -- models/care_site.sql
 
+WITH CareSiteData AS (
+    SELECT
+        REPLACE(JSON_EXTRACT(o.data, '$.id'), '"', '') AS care_site_id,
+        REPLACE(JSON_EXTRACT(o.data, '$.name'), '"', '') AS care_site_name,
+        REPLACE(JSON_EXTRACT(o.data, '$.id'), '"', '') AS care_site_source_value,
+        REPLACE(JSON_EXTRACT(o.data, '$.type[0].coding[0].display'), '"', '') AS place_of_service_source_value
+    FROM {{ source('raw', 'Organization') }} o
+),
+LocationData AS (
+    SELECT
+        REPLACE(JSON_EXTRACT(l.data, '$.managingOrganization.identifier.value'), '"', '') AS managingOrganization,
+        REPLACE(JSON_EXTRACT(l.data, '$.id'), '"', '') AS location_id
+    FROM {{ source('raw', 'Location') }} l
+)
+
 SELECT
-    REPLACE(JSON_EXTRACT(data, '$.id'), '"', '') AS care_site_id,
-    REPLACE(JSON_EXTRACT(data, '$.name'), '"', '') AS care_site_name,
+    csd.care_site_id,
+    csd.care_site_name,
     8717 AS place_of_service_concept_id,
-    NULL AS location_id,
-    REPLACE(JSON_EXTRACT(data, '$.id'), '"', '') AS care_site_source_value,
-    REPLACE(JSON_EXTRACT(data, '$.type[0].coding[0].display'), '"', '') AS place_of_service_source_value
-FROM {{ source('raw', 'Organization') }}
+    COALESCE(ld.location_id, NULL) AS location_id,
+    csd.care_site_source_value,
+    csd.place_of_service_source_value
+FROM CareSiteData csd
+LEFT JOIN LocationData ld ON csd.care_site_id = ld.managingOrganization
