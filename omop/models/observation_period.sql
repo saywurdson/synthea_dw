ff8556d0143b36c44587b391b2999383610f40f3
saywurdson <34385409+saywurdson@users.noreply.github.com>
2024-01-09 23:01:50 -0500

    observation_period id update

--- omop/models/observation_period.sql
+++ omop/models/observation_period.sql
@@ -1,15 +1,16 @@
 WITH ObservationPeriod AS (
     SELECT 
         person_id,
+        observation_id,
         observation_source_value,
         MIN(observation_datetime) AS observation_period_start_datetime,
         MAX(observation_datetime) AS observation_period_end_datetime
     FROM {{ ref('observation') }}
-    GROUP BY person_id, observation_source_value
+    GROUP BY person_id, observation_source_value, observation_id
 )
 
 SELECT 
-    ROW_NUMBER() OVER (ORDER BY person_id, observation_period_start_datetime) AS observation_period_id,
+    observation_id || '-' || LOWER(SUBSTR(HEX(RANDOM()), 1, 8)) AS observation_period_id,
     person_id,
     CAST(observation_period_start_datetime AS DATE) AS observation_period_start_date,
     CAST(observation_period_end_datetime AS DATE) AS observation_period_end_date,
