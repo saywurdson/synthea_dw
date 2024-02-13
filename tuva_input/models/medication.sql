8e051775cb54b6fe20f69d1a11dfc35f6bb28787
saywurdson <34385409+saywurdson@users.noreply.github.com>
2024-02-13 00:32:18 -0500

    finished medication and observation

--- tuva_input/models/medication.sql
+++ tuva_input/models/medication.sql
@@ -19,7 +19,7 @@ SELECT DISTINCT
         AND c2.vocabulary_id = 'NDC'
         AND c2.domain_id = 'Drug'
         AND c2.invalid_reason IS NULL
-        AND c1.concept_class_id in ('Branded Drug', 'Clinical Drug', 'Quant Clinical Drug')
+        AND c1.concept_class_id in ('Branded Drug', 'Clinical Drug', 'Quant Clinical Drug', 'Quant Branded Drug')
         AND LENGTH(c2.concept_code) = 11
         ORDER BY c2.concept_id
         LIMIT 1
@@ -34,7 +34,7 @@ SELECT DISTINCT
         AND c2.vocabulary_id = 'NDC'
         AND c2.domain_id = 'Drug'
         AND c2.invalid_reason IS NULL
-        AND c1.concept_class_id in ('Branded Drug', 'Clinical Drug', 'Quant Clinical Drug')
+        AND c1.concept_class_id in ('Branded Drug', 'Clinical Drug', 'Quant Clinical Drug', 'Quant Branded Drug')
         AND LENGTH(c2.concept_code) = 11
         ORDER BY c2.concept_id
         LIMIT 1
@@ -42,20 +42,44 @@ SELECT DISTINCT
     REPLACE(JSON_EXTRACT(m, '$.medicationCodeableConcept.coding[0].code'), '"', '') AS rxnorm_code,
     REPLACE(JSON_EXTRACT(m, '$.medicationCodeableConcept.coding[0].display'), '"', '') AS rxnorm_description,
     r."atc class id" AS atc_code,
-    r.rxstring AS atc_description,
+    (
+        SELECT c3.concept_name
+        FROM {{ source('vocabulary', 'concept') }} c3
+        WHERE c3.concept_code = atc_code
+            AND c3.vocabulary_id = 'ATC'
+            AND c3.domain_id = 'Drug'
+            AND c3.invalid_reason IS NULL
+            AND c3.standard_concept = 'C'
+        LIMIT 1
+    ) AS atc_description,
     NULL AS route,
     CASE
         WHEN ds.amount_value IS NOT NULL THEN ds.amount_value
-        WHEN ds.amount_value IS NULL AND ds.numerator_value IS NOT NULL AND ds.denominator_value IS NULL THEN ds.numerator_value
-        WHEN ds.amount_value IS NULL AND ds.numerator_value IS NOT NULL AND ds.denominator_value IS NOT NULL THEN ds.numerator_value / ds.denominator_value
-        ELSE 1
+        WHEN ds.numerator_value IS NOT NULL THEN
+            CASE
+                WHEN ds.denominator_value IS NOT NULL AND ds.denominator_value != 0 THEN ds.numerator_value / ds.denominator_value
+                ELSE ds.numerator_value
+            END
+        ELSE NULL
     END AS strength,
     CASE
         WHEN REPLACE(JSON_EXTRACT(m, '$.dosageInstruction[0].doseAndRate[0].doseQuantity.value'), '"', '') IS NOT NULL 
         THEN CAST(REPLACE(JSON_EXTRACT(m, '$.dosageInstruction[0].doseAndRate[0].doseQuantity.value'), '"', '') AS INTEGER) * 30
         ELSE 1
     END AS quantity,
-    NULL AS quantity_unit,
+    (
+        SELECT c4.concept_name
+        FROM {{ source('vocabulary', 'concept') }} c1
+        JOIN {{ source('vocabulary', 'drug_strength') }} ds ON c1.concept_id = ds.drug_concept_id
+        JOIN {{ source('vocabulary', 'concept') }} c4 ON c4.concept_id = COALESCE(ds.amount_unit_concept_id, ds.numerator_unit_concept_id)
+        WHERE c1.concept_code = REPLACE(JSON_EXTRACT(m, '$.medicationCodeableConcept.coding[0].code'), '"', '')
+            AND c1.vocabulary_id = 'RxNorm'
+            AND c1.domain_id = 'Drug'
+            AND c1.invalid_reason IS NULL
+            AND c1.standard_concept = 'S'
+            AND c4.concept_id IS NOT NULL
+        LIMIT 1
+    ) AS quantity_unit,
     CASE
         WHEN REPLACE(JSON_EXTRACT(m, '$.dosageInstruction[0].doseAndRate[0].doseQuantity.value'), '"', '') IS NOT NULL 
         THEN 30
